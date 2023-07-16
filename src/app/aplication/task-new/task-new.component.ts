1b8d9254c138cdf8e7dac800c5a641990a4f36b0
unknown <eric_pereira04@hotmail.com>
2023-07-16 14:15:38 +0100

    development of Page new Task - creation of form

--- src/app/aplication/task-new/task-new.component.ts
+++ src/app/aplication/task-new/task-new.component.ts
@@ -28,6 +28,13 @@ export class TaskNewComponent {
     extra: new FormControl(''),
   });
 
+  public errorMessages = {
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: '',
+  };
+
   constructor(private route: ActivatedRoute) {}
 
   /** Navigate to the previous page according to the browser history
@@ -35,6 +42,50 @@ export class TaskNewComponent {
   returnPreviousPage() {
     try {
       history.back();
+    } catch (error) {}
+  }
+
+  /** Handle the loss of focus from the input
+   * @param controlName - control from the form that lost focus
+   */
+  InputLostFocus(controlName: string) {
+    try {
+      // Get the details of the control interacted
+      const controlDetails = (this.newForm.controls as any)[controlName];
+
+      // If the status is Valid, cancel this method
+      if (controlDetails.status == 'VALID') return;
+
+      // set the error message according to the error identified
+      switch (Object.keys(controlDetails.errors)[0]) {
+        case 'minlength':
+          (this.errorMessages as any)[controlName] =
+            'Required at least ' +
+            controlDetails.errors.minlength.requiredLength +
+            ' characters.';
+          break;
+        case 'required':
+          (this.errorMessages as any)[controlName] = 'This field is mandatory.';
+          break;
+
+        case 'ngbDate':
+          if (Object.keys(controlDetails.errors.ngbDate.invalid)[0])
+            (this.errorMessages as any)[controlName] = 'Invalid format.';
+          break;
+
+        default:
+          break;
+      }
+    } catch (error) {}
+  }
+
+  /** Handle the gain of Focus on the Inputbox
+   * @param controlName - name of the control interacted
+   */
+  InputGainedFocus(controlName: string) {
+    try {
+      // Clears the current error message
+      (this.errorMessages as any)[controlName] = '';
     } catch (error) {}
   }
 }
